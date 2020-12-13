403f7b651aeeb19a4d0ae4f34332b79b8eb1f13a
Jula90210 <Jula90210@users.noreply.github.com>
2020-12-13 10:46:17 +0000

    Change1

--- program5.py
+++ program5.py
@@ -1,16 +1,16 @@
 #Zad.4. Policz ile kilometrow przejechaly samochody:
 
 koszyk = [
-    {'produkt': 'kia', 'ilosc': 1, 'km': 23.5},
-    {'produkt': 'porsche', 'ilosc': 3, 'km': 22.5},
-    {'produkt': 'fiat126p', 'ilosc': 4, 'km': 31.5}
+    {'samochod': 'kia', 'ilosc': 1, 'km': 23.5},
+    {'samochod': 'porsche', 'ilosc': 3, 'km': 22.5},
+    {'samochod': 'fiat126p', 'ilosc': 4, 'km': 31.5}
 ]
 
 suma = 0
 for poz in koszyk:
     il = poz['ilosc']
-    c = poz['km']
-    suma = suma + (c * il)
+    km = poz['km']
+    suma = suma + (km * il)
     # print(c)
     # print(suma)
     # print(poz)
